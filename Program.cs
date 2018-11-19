4bd272e15dd461d6c894ca2505fee81be799bc4b
LimestaX <45181474+LimestaX@users.noreply.github.com>
2018-11-19 15:14:10 -0500

    Project Creation
    Initiated Project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+//program dedicated to communicating with NodeJS and initiating localtunnel
+namespace NodeJS_IPC
+{
+    class Program
+    {
+        //In testing phase we will communicate with NodeJS and ensure commands get sent through 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
